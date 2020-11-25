d096f8e13e08287b8ba66d296b9c78885f8fba96
Mahesh Bisl <bisl.mahesh@gmail.com>
2020-11-24 21:45:16 -0500

    add spouse

--- test.py
+++ test.py
@@ -8,9 +8,14 @@ def print_family():
 
 # print(family)
 assert add_child("Queen Margret", "Bill", "Male") == True
+assert add_child("Queen Margret", "Charlie", "Male") == True
+assert add_child("Queen Margret", "Percy", "Male") == True
+assert add_child("Queen Margret", "Ronald", "Male") == True
+assert add_child("Queen Margret", "Ginerva", "Female") == True
 # print_family()
 assert get_relationship("Queen Margret", "Spouse") == "King Arthur"
 
 assert add_spouse("Bill", "Flora") == True
 print_family()
 assert get_relationship("Flora", "Spouse") == "Bill"
+
